e5a3191d655b665aa4e5b38bf522e58616892ace
V2Valdecantos <99716006+V2Valdecantos@users.noreply.github.com>
2025-01-12 15:07:42 +0800

    SFML Project Setup

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Vito Valdecantos - GDPARCM
+
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+sf::RenderWindow window;
+
+int main()
+{
+	
+	std::cout << "TEST" << std::endl;
+
+	return 0;
+}
