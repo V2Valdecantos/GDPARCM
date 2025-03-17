a806ee7457d97cc448017b73564f1d28f4cf088d
V2Valdecantos <99716006+V2Valdecantos@users.noreply.github.com>
2025-03-18 00:21:20 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
-#include "BaseRunner.h"
-int main() {
-	BaseRunner runner;
-	runner.run();
+#include "mutex"
+#include "string"
+#include "thread"
+
+
+std::mutex guard;
+
+
+void printMessage(int id, std::string message)
+{
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+
+	guard.lock();
+	std::cout << "Thread [" << id << "]: " << message << std::endl;
+	guard.unlock();
 }
+
+int main() 
+{
+	////join
+	//for (int i = 0; i < 12; i++) 
+	//{
+	//	std::thread t = std::thread(printMessage, i, "Join Finished Execution");
+	//	t.join();
+	//}
+
+	//detach
+	for (int i = 0; i < 12; i++)
+	{
+		std::thread t = std::thread(printMessage, i, " Detach Finished Execution");
+		t.detach();
+	}
+
+	std::this_thread::sleep_for(std::chrono::seconds(20));
+}
+
